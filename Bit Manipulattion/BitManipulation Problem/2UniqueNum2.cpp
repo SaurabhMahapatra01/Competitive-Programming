42f270ba8abc7666331e8847f3af1b3480e344d3
Saurabh_01 <saurabhmahapatra001@gmail.com>
2023-01-10 11:22:10 +0530

    Unique Num2

---
+++ Bit Manipulattion/BitManipulation Problem/2UniqueNum2.cpp
@@ -0,0 +1,18 @@
+/*
+ Unique Number - Given 2n + 2 Numbers,
+ where every number is coming twice expect
+ two numbers find those two Unique numbers
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+
+   return 0;
+}
