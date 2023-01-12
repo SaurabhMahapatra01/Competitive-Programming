47b5369156c0d07c14a6504a9cfbb6ae271376e3
Saurabh_01 <saurabhmahapatra001@gmail.com>
2023-01-12 22:35:43 +0530

    Q17  A. Domino piling

---
+++ TLE/Week1/Que17.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+   #endif
+	int a, b;
+	cin>>a>>b;
+	cout<<(a*b)/2<<endl;
+
+	return 0;
+}
